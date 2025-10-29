6f8998c691d914f898b9d0faf4f2d9de1a0de517
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2025-10-29 12:45:42 +0100

    Implemented lexer/tokenizer

---
+++ src/lexer.rs
@@ -0,0 +1,183 @@
+#[derive(Debug, PartialEq)]
+pub enum Token {
+    Identifier(String),
+    Number(i32),
+    Let,
+    Loop,
+    Plus,
+    Equals,
+    Semicolon,
+    OpenBracket,
+    CloseBracket,
+    Print,
+}
+
+pub struct Lexer {
+    src: String,
+    pos: usize,
+}
+
+impl Lexer {
+    pub fn new(src: String) -> Self {
+        Lexer { src, pos: 0 }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.current_char() {
+            if c.is_whitespace() {
+                self.pos += 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn current_char(&self) -> Option<char> {
+        self.src.chars().nth(self.pos)
+    }
+
+    fn try_parse_identifier(&mut self) -> Option<Token> {
+        let start = self.pos;
+        let Some(c) = self.current_char() else {
+            return None;
+        };
+        if !c.is_alphabetic() {
+            return None;
+        }
+        self.pos += 1;
+        while let Some(c) = self.current_char() {
+            if !c.is_alphanumeric() {
+                break;
+            } else {
+                self.pos += 1;
+            }
+        }
+        let identifier = &self.src[start..self.pos];
+        match identifier {
+            "let" => Some(Token::Let),
+            "loop" => Some(Token::Loop),
+            "print" => Some(Token::Print),
+            _ => Some(Token::Identifier(identifier.to_string())),
+        }
+    }
+
+    fn try_parse_number(&mut self) -> Option<Token> {
+        let start = self.pos;
+        let Some(c) = self.current_char() else {
+            return None;
+        };
+        if !c.is_digit(10) {
+            return None;
+        }
+        self.pos += 1;
+        while let Some(c) = self.current_char() {
+            if !c.is_digit(10) {
+                break;
+            } else {
+                self.pos += 1;
+            }
+        }
+        let number_str = &self.src[start..self.pos];
+        Some(Token::Number(number_str.parse::<i32>().unwrap()))
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.skip_whitespace();
+        let current_char = self.current_char()?;
+        if let Some(token) = self.try_parse_identifier() {
+            return Some(token);
+        }
+        if let Some(token) = self.try_parse_number() {
+            return Some(token);
+        }
+        if let Some(token) = match current_char {
+            '+' => Some(Token::Plus),
+            '=' => Some(Token::Equals),
+            ';' => Some(Token::Semicolon),
+            '{' => Some(Token::OpenBracket),
+            '}' => Some(Token::CloseBracket),
+            _ => None,
+        } {
+            self.pos += 1;
+            return Some(token);
+        }
+        panic!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fibonacci_program() {
+        let input =
+            "let a = 0; let b = 1; let c = 0; loop 5 { c = a + b; a = b; b = c; }; print c;";
+
+        let expected_tokens = vec![
+            // let a = 0;
+            Token::Let,
+            Token::Identifier("a".to_string()),
+            Token::Equals,
+            Token::Number(0),
+            Token::Semicolon,
+            // let b = 1;
+            Token::Let,
+            Token::Identifier("b".to_string()),
+            Token::Equals,
+            Token::Number(1),
+            Token::Semicolon,
+            // let c = 0;
+            Token::Let,
+            Token::Identifier("c".to_string()),
+            Token::Equals,
+            Token::Number(0),
+            Token::Semicolon,
+            // loop 5 {
+            Token::Loop,
+            Token::Number(5),
+            Token::OpenBracket,
+            // c = a + b;
+            Token::Identifier("c".to_string()),
+            Token::Equals,
+            Token::Identifier("a".to_string()),
+            Token::Plus,
+            Token::Identifier("b".to_string()),
+            Token::Semicolon,
+            // a = b;
+            Token::Identifier("a".to_string()),
+            Token::Equals,
+            Token::Identifier("b".to_string()),
+            Token::Semicolon,
+            // b = c;
+            Token::Identifier("b".to_string()),
+            Token::Equals,
+            Token::Identifier("c".to_string()),
+            Token::Semicolon,
+            // };
+            Token::CloseBracket,
+            Token::Semicolon,
+            // print c;
+            Token::Print,
+            Token::Identifier("c".to_string()),
+            Token::Semicolon,
+        ];
+
+        let lexer = Lexer::new(input.to_string());
+        let actual_tokens: Vec<Token> = lexer.collect();
+
+        assert_eq!(actual_tokens, expected_tokens);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_stops_at_invalid_char() {
+        let input = "let x = @123";
+        let lexer = Lexer::new(input.to_string());
+        let _: Vec<Token> = lexer.collect();
+    }
+}

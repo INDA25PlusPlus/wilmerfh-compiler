b2cb41ff8ec1bd4e18fcca870e2cf732d8db8b70
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2025-10-30 09:54:57 +0100

    Added main file and ability to print ast

--- src/ast.rs
+++ src/ast.rs
@@ -1,38 +1,46 @@
 #![allow(dead_code)]
 
+#[derive(Debug)]
 pub enum Term {
     Identifier(String),
     Number(i32),
 }
 
+#[derive(Debug)]
 pub struct Expr {
     pub lhs: Term,
     pub rhs: Option<Box<Expr>>,
 }
 
+#[derive(Debug)]
 pub struct LetStatement {
     pub identifier: String,
     pub value: Expr,
 }
 
+#[derive(Debug)]
 pub struct AssignmentStatement {
     pub identifier: String,
     pub value: Expr,
 }
 
+#[derive(Debug)]
 pub struct Block {
     pub statements: Box<StatementList>,
 }
 
+#[derive(Debug)]
 pub struct LoopStatement {
     pub count: Expr,
     pub body: Box<Block>,
 }
 
+#[derive(Debug)]
 pub struct PrintStatement {
     pub value: Expr,
 }
 
+#[derive(Debug)]
 pub enum Statement {
     Let(LetStatement),
     Assignment(AssignmentStatement),
@@ -40,10 +48,12 @@ pub enum Statement {
     Print(PrintStatement),
 }
 
+#[derive(Debug)]
 pub struct StatementList {
     pub statements: Vec<Statement>,
 }
 
+#[derive(Debug)]
 pub struct AbstractSyntaxTree {
     pub statements: StatementList,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-mod lexer;
+use std::env;
+use std::fs;
+
 mod ast;
+mod lexer;
 mod parser;
 
-fn main() {}
+use lexer::Lexer;
+use parser::Parser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} [--ast] <file>", args[0]);
+        return Ok(());
+    }
+
+    let print_ast_flag = args.contains(&"--ast".to_string());
+    let file_path = args.last().unwrap();
+
+    let content = fs::read_to_string(file_path)?;
+
+    let lexer = Lexer::new(content);
+    let tokens: Vec<_> = lexer.collect();
+
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse();
+
+    if print_ast_flag {
+        println!("{:?}", &ast);
+    }
+
+    Ok(())
+}
